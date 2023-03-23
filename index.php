69d56facc151f859a17a0fe3c0598b1bf36723bd
Rouela D <81519103+angelorenz70@users.noreply.github.com>
2023-03-23 11:50:52 +0800

    done label

--- index.php
+++ index.php
@@ -31,27 +31,35 @@
                     <!--FORM-->
                     <form method="POST" action="prediction.php">
                         <div class="input-group">
+                            <label for="">Enter Age: </label>
                             <input class="input--style-1" type="number" step="1" min="0" placeholder="Age" name="Age">
                         </div>
                         <div class="input-group">
+                            <label for="">(0 - None, 1 - pre-diabetes, 2 - have diabetes)</label>
                             <input class="input--style-1" type="number" step="1" min="0" max="2" placeholder="Diabetes" name="Diabetes_012">
                         </div>
                         <div class="input-group">
+                            <label for="">(0 - None, 1 - high blood pressure)</label>
                             <input class="input--style-1" type="number" step="1" min="0" max="1" placeholder="High Blood Pressure" name="HighBP">
                         </div>
                         <div class="input-group">
+                            <label for="">(0 - None, 1 - High Cholesterol)</label>
                             <input class="input--style-1" type="number" step="1" min="0" max="1" placeholder="High Cholesterol" name="HighChol">
                         </div>
                         <div class="input-group">
+                            <label for="">(0 - None, 1 - Check Cholesterol)</label>
                             <input class="input--style-1" type="number" step="1" min="0" max="1" placeholder="Cholesterol Check" name="CholCheck">
                         </div>
                         <div class="input-group">
+                            <label for="">Input BMI: </label>
                             <input class="input--style-1" type="number" step="0.1" min="1" placeholder="Body Mass Index" name="BMI">
                         </div>
                         <div class="input-group">
+                            <label for="">(0 - Not smoker, 1 - Smoker)</label>
                             <input class="input--style-1" type="number" step="1" min="0" max="3" placeholder="Smoker" name="Smoker">
                         </div>
                         <div class="input-group">
+                            <label for="">(0 - None, 1 - Stroke)</label>
                             <input class="input--style-1" type="number" step="1" min="0" max="3" placeholder="Stroke" name="Stroke">
                         </div>
                         
